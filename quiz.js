4bbd944c708c866368f3aefe2804096f4f722f5b
Shivam-202 <83532229+Shivam-202@users.noreply.github.com>
2022-02-09 01:41:00 -0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -242,6 +242,7 @@ function ansfunction() {
   *********************************************************************  */
 
 function subfunction() {
+    document.getElementById('timer').style="display:none";
     document.getElementById('submitbtn').disabled = true;
     document.getElementById('nextbtn').disabled = true;
     document.getElementById('nextbtn').style.backgroundColor = "rgb(87, 152, 218)";
